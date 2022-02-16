3b180113310a4b9b681f792bcef9c8ef42bcd7b6
BenC0 <suddensquirrel94@gmail.com>
2022-02-16 09:31:26 +0000

    Converted ab_test_config to JS file to allow for dynamic JS conditions

---
+++ src/ab_test_config.js
@@ -0,0 +1,10 @@
+export default {
+    "id": "PAH000",
+    "tracking": {
+        "google_analytics": {
+            "dimension": false
+        },
+        "hotjar": false
+    },
+    "conditions": []
+}
