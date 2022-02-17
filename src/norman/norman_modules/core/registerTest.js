8c4d2a568fea8201a1e302bd46861d27a70edcf0
BenC0 <suddensquirrel94@gmail.com>
2022-02-17 07:55:07 +0000

    Added commenting to registerTest

--- src/norman/norman_modules/core/registerTest.js
+++ src/norman/norman_modules/core/registerTest.js
@@ -1,4 +1,11 @@
-export default function registerTest(testID, variant, extraDetails) {
+/**
+ * Registers a test in the window.norman object for logging and debugging purposes
+ * @param {string} testID A string to identify the test, this is consistent with the CSS "test_loaded" body class and test tracking ID
+ * @param {object} variant An object of various details specific to the variant that has loaded. A common use case is the variant name/id 
+ * @param {object} [extraDetails={}] An object of additional details. Does not need to be variant specific. Defaults to an empty object. 
+ * @returns {object} Returns the stored test from the window.norman object
+ */
+export default function registerTest(testID, variant, extraDetails = {}) {
 	let testConfig = {
 		variant,
 		id: testID,
