a29fa99eb6e73618af3d6465aaa317691e4bedd3
BenC0 <suddensquirrel94@gmail.com>
2022-02-17 08:59:01 +0000

    Tweaked norman output

--- src/norman/index.js
+++ src/norman/index.js
@@ -2,6 +2,7 @@
 import * as config from "./config.js"
 // Core Modules
 import log from './norman_modules/core/log.js';
+import track from './norman_modules/core/tracking.js';
 import poll from './norman_modules/core/pollFunction.js';
 import cookie from './norman_modules/core/cookieFunctions.js';
 import registerTest from './norman_modules/core/registerTest.js';
@@ -14,18 +15,23 @@ import onMouseLeave from './norman_modules/utils/onMouseLeave.js';
 import watchForChange from './norman_modules/utils/watchForChange.js';
 import getHighestZIndex from './norman_modules/utils/getHighestZIndex.js';
 
-export {
-	log,
-	poll,
-	track,
-	cookie,
+export const norman = {
 	config,
-	debounce,
-	registerTest,
-	isInViewport,
-	onMouseLeave,
-	watchForChange,
-	getHighestZIndex,
-	elementManagement,
+	core: {
+		log,
+		poll,
+		track,
+		cookie,
+		registerTest,
+		elementManagement,
+	},
+	utils: {
+		debounce,
+		isInViewport,
+		onMouseLeave,
+		watchForChange,
+		getHighestZIndex,
+	},
 }
 
+export default norman 
