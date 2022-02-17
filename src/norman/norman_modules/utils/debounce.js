4005bfa4c6b82c04d24ec2ea3d126a30e7ab5c4d
BenC0 <suddensquirrel94@gmail.com>
2022-02-17 08:15:21 +0000

    Converted debounce commenting to JSDoc format

--- src/norman/norman_modules/utils/debounce.js
+++ src/norman/norman_modules/utils/debounce.js
@@ -1,7 +1,9 @@
-// Returns a function, that, as long as it continues to be invoked, will not
-// be triggered. The function will be called after it stops being called for
-// N milliseconds. If `immediate` is passed, trigger the function on the
-// leading edge, instead of the trailing.
+/**
+ * Returns a function, that, as long as it continues to be invoked, will not be triggered.
+ * @param {function} func The function to be called 
+ * @param {integer} wait The millisecodns to wait for after this function stops being called
+ * @param {boolean} immediate If true, the funciton will be triggered on the leading edge instead of the trailing 
+ */
 export default function debounce(func, wait, immediate) {
 	var timeout;
 	return function() {
