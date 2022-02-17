05bb968aa9e2aaab00af4886cc2e62114fbb1c1a
BenC0 <suddensquirrel94@gmail.com>
2022-02-17 07:57:15 +0000

    Added comments to log

--- src/norman/norman_modules/core/log.js
+++ src/norman/norman_modules/core/log.js
@@ -1,3 +1,8 @@
+/**
+ * Logs a message to the specified test in the window.norman object
+ * @param {string} testID The ID of the test (Usually pulled dynamically from the ab_test_config file) 
+ * @param {*} msg The message to be logged, can be any type but strings and objects are most common.
+ */
 export default function log(testID, msg) {
 	let date = new Date
 	window.norman[testID].logs.push({
